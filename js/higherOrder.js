b4edab826d50c55b3ba1d6edbdf4e0bbafd049ee
utsav-inheritx <utsav.k@inheritx.com>
2024-06-25 17:28:24 +0530

    hoisting

--- js/higherOrder.js
+++ js/higherOrder.js
@@ -1,35 +1,61 @@
 const radius = [3, 1, 2, 4];
 
-const calculateArea = function(radius){
+const area = function(radius){
+    return Math.PI * radius * radius;
+};
+
+const circumference = function(radius){
+    return 2 * Math.PI * radius;
+};
+
+const diameter = function(radius){
+    return 2 * radius;
+};
+
+const calcualate = function(radius, logic){
     const output = [];
-    for (let i = 0; i < radius.length; i++){
-        output.push(Math.PI * radius[i] * radius[i]);
+    for(let i = 0; i < radius.length; i++){
+        output.push(logic(radius[i]));
     }
     return output;
 };
-console.log(calculateArea(radius));
+
+console.log(calcualate(radius, area));
+console.log(calcualate(radius, circumference));
+console.log(calcualate(radius, diameter));
 
 
 
-const calculateCircumference = function(radius){
-    const output = [];
-    for (let i = 0; i < radius.length; i++){
-        output.push(2 * Math.PI * radius[i]);
-    }
-    return output;
-};
-console.log(calculateCircumference(radius));
+// const calculateArea = function(radius){
+//     const output = [];
+//     for (let i = 0; i < radius.length; i++){
+//         output.push(Math.PI * radius[i] * radius[i]);
+//     }
+//     return output;
+// };
+// console.log(calculateArea(radius));
 
 
 
-const calculateDiameter = function(radius){
-    const output = [];
-    for (let i = 0; i < radius.length; i++){
-        output.push(2 * radius[i]);
-    }
-    return output;
-};
-console.log(calculateDiameter(radius));
+// const calculateCircumference = function(radius){
+//     const output = [];
+//     for (let i = 0; i < radius.length; i++){
+//         output.push(2 * Math.PI * radius[i]);
+//     }
+//     return output;
+// };
+// console.log(calculateCircumference(radius));
+
+
+
+// const calculateDiameter = function(radius){
+//     const output = [];
+//     for (let i = 0; i < radius.length; i++){
+//         output.push(2 * radius[i]);
+//     }
+//     return output;
+// };
+// console.log(calculateDiameter(radius));
 
 
 
---
+++ js/hoisting.js
@@ -0,0 +1,11 @@
+
+x = 10;
+console.log(x);
+var x;
+
+
+
+console.log(sum(10,20));
+function sum(a,b){
+    return a+b;
+}
